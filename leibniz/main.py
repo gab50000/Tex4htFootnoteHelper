f17e3940658cbad7ee0e13cb131364a2652b2ba7
gk <gabriel.kabbe@mail.de>
2016-11-23 21:43:12 +0100

    Update main

--- leibniz/main.py
+++ leibniz/main.py
@@ -7,9 +7,11 @@ import plac
 def pdflatex(latex_file):
     """Run pdflatex without interruption"""
     child = pexpect.spawn("/usr/bin/pdflatex", [latex_file])
-    child.logfile = sys.stdout
+    # child.logfile = sys.stdout
     child.expect_exact("?")
     child.sendline("R")
+    print(child.before.decode("utf-8"))
+    print(child.after.decode("utf-8"))
 
 
 def main(*args):
