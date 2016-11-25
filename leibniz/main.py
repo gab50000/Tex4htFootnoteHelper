18c478f887dd7335aff9f4404f5865305c343d35
Gabriel Kabbe <gabriel.kabbe@mail.de>
2016-11-25 21:43:03 +0100

    set timeout to 300

--- leibniz/main.py
+++ leibniz/main.py
@@ -13,12 +13,16 @@ def pdflatex():
     then run pdflatex without interruption.
     Finally, call find_all_footnotes_in_html"""
     
+    print("Set footnote markers")
     latex_file = set_footnote_markers_in_tex.main()
-    child = pexpect.spawn("/usr/bin/htlatex", [latex_file, "xhtml"])
+    print("Compile Latex to HTML")
+    child = pexpect.spawn("/usr/bin/htlatex", [latex_file, "xhtml"], timeout=300)
     for i in range(3):
         child.expect_exact("?")
         child.sendline("R")
     print(child.read())
+    child.close()
+    print("Modify HTML")
     find_all_footnotes_in_html.main()
 
 
